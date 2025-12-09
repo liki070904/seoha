79a2f9b7cb0d4fb7de3a9066b6b677d822d60c0c
seoha <liki070904@gmail.com>
2025-12-09 09:36:50 +0900

    2511?? 독후활동지 별도 URL 분리 생성 검증

--- moonji/testcase/Book_Report_URL_Validation.py
+++ moonji/testcase/Book_Report_URL_Validation.py
@@ -6,15 +6,18 @@ from module.homepage_common_functions import (intro_skip, navigation_moonji)
 
 logger = logging.getLogger()
 
+# dev : dev-moonji.ntoday.kr
+# real : moonji.com
+
 def main():
     driver, wait = setup_driver()
     try:
-        # ✅ FSS 관리자 로그인
-        driver.get("https://dev-moonji.ntoday.kr")
+        # ✅ 로그인
+        driver.get("https://moonji.com")
         intro_skip(driver, wait)
         navigation_moonji(driver, wait, "도서/저자", "자료실")
         fill_form_field(driver, wait, "//button[@data-key='category' and @data-value='32']", None, field_type="click", ui_name="독후활동지 버튼")
-        verify_url(driver, "https://dev-moonji.ntoday.kr/bookAndAuthor/dataroom/list?page=1&type=32", exact=True)
+        verify_url(driver, "https://moonji.com/bookAndAuthor/dataroom/list?page=1&type=32", exact=True)
 
         # ✅ 테스트 완료 알림
         pyautogui.confirm(
